b2ef9e99ed565ab199d2d3fb8741cb591f3884c8
dabsmanuel <dabsmanuel5015@gmail.com>
2024-10-14 22:51:07 +0100

    feat: added testimonial, footer and an animated counter component, installed dependencies, and aaded images

---
+++ src/components/Home/Testimonials/Testimonials.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import TestimonialCarousel from './TestimonialCarousel'
+
+const Testimonials = () => {
+  return (
+    <div className='custom-container py-8'>
+      <h1 className='lg:text-[40px] text-[32px]'>Testimonies</h1>
+      <div>
+        <TestimonialCarousel/>
+      </div>
+    </div>
+  )
+}
+
+export default Testimonials
