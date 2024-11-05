0741dd5fcec598d1c0610bfa8610f41f69c3aca6
dabsmanuel <dabsmanuel5015@gmail.com>
2024-11-05 19:38:40 +0100

    docs: reduced gallery image sizes and added box shadow to animated counter component

--- src/components/Home/counter/AnimatedCounter.tsx
+++ src/components/Home/counter/AnimatedCounter.tsx
@@ -55,25 +55,31 @@ const AnimatedCounter = () => {
   ];
 
   return (
-    <div ref={containerRef} className="lg:h-[20rem] h-[10rem] bg-[rgba(254,248,242,0.5)]">
+    <div
+      ref={containerRef}
+      className="lg:h-[20rem] h-[10rem] bg-[rgba(254,248,242,0.5)]"
+    >
       <div className="flex items-center justify-center h-full space-x-[2rem] custom-container">
         {counterItems.map((item, index) => (
-          <div key={index} className="text-center shadow-[0_14px_28px_rgba(0,0,0,0.25),0_10px_10px_rgba(0,0,0,0.22)] p-2 lg:p-4 rounded">
-          <div className="text-[20px] lg:text-[48px] flex items-center justify-center mb-[1.3rem] lg:mb-0">
-            {isVisible && (
-              <CountUp
-                start={0}
-                end={item.end}
-                duration={item.duration}
-                separator=","
-              />
-            )}
-            {item.suffix && (
-              <p className="text-[20px] lg:text-[48px]">{item.suffix}</p>
-            )}
+          <div
+            key={index}
+            className="text-center shadow-[0_14px_28px_rgba(0,0,0,0.25),0_10px_10px_rgba(0,0,0,0.22)] p-2 lg:p-4 rounded"
+          >
+            <div className="text-[20px] lg:text-[48px] flex items-center justify-center mb-[1.3rem] lg:mb-0">
+              {isVisible && (
+                <CountUp
+                  start={0}
+                  end={item.end}
+                  duration={item.duration}
+                  separator=","
+                />
+              )}
+              {item.suffix && (
+                <p className="text-[20px] lg:text-[48px]">{item.suffix}</p>
+              )}
+            </div>
+            <h3 className="mb-2 text-[14px] lg:text-[28px]">{item.label}</h3>
           </div>
-          <h3 className="mb-2 text-[14px] lg:text-[28px]">{item.label}</h3>
-        </div>
         ))}
       </div>
     </div>
