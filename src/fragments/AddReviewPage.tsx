122e5ce0de698a83181c055ac734f0a83aeadec1
dabsmanuel <dabsmanuel5015@gmail.com>
2024-11-26 00:49:18 +0100

    feat: built a review form and review display page and incorporated backend api

---
+++ src/fragments/AddReviewPage.tsx
@@ -0,0 +1,10 @@
+import MainReview from "@/components/Review/MainReview";
+import React from "react";
+
+export const ReviewPage = () => {
+  return (
+    <div>
+      <MainReview />
+    </div>
+  );
+};
---
+++ src/fragments/ReadReviewsFragmentPage.tsx
@@ -0,0 +1,13 @@
+import ReviewsDisplay from "@/components/Review/ReviewsDisplay";
+import SwiperComp from "@/components/shared/Swiper/SwiperComp";
+
+const ReadReviewsFragmentPage = () => {
+  return (
+    <>
+      <ReviewsDisplay />
+      <SwiperComp />
+    </>
+  );
+};
+
+export default ReadReviewsFragmentPage;
